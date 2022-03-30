be21e38500c992c5381b9163d8700a4b9a447243
Paul <paul.inglsmit@gmail.com>
2022-03-30 10:04:04 +0300

    finish text-box

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bd97a3f50af4957736d8b1706a137f87');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5726a4218fb53364c2cfe655206da029');
